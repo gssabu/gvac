e7b46dba35cd64094d390ded8032b7106dbe5e41
gssabu <87628438+gssabu@users.noreply.github.com>
2022-01-26 20:20:52 -0600

    Update lino_base_node.cpp

--- src/lino_base_node.cpp
+++ src/lino_base_node.cpp
@@ -1,8 +1,8 @@
-#include "lino_base.h"
+#include "gvac_base.h"
 
 int main(int argc, char** argv )
 {
-    ros::init(argc, argv, "lino_base_node");
+    ros::init(argc, argv, "gvac_base_node");
     LinoBase lino;
     ros::spin();
     return 0;
